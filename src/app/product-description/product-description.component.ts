94edf31a06c13a8da3ff1c5aa201cdec04c35d90
Vijeth Sarashetti <M1053749@mindtree.com>
2020-11-29 23:53:22 +0530

    Task 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+import {ProductService} from '../product.service';
+imports : [
+  ProductService
+]
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
 
-  constructor() { }
+export class ProductDescriptionComponent implements OnInit {
+private albumInfo : any;
+  constructor(private _productService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
