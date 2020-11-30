852541c0ef157352db09fee2406e9a0d765ca9a0
Vijeth Sarashetti <M1053749@mindtree.com>
2020-11-30 14:21:21 +0530

    Task 6 complete

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import {Product} from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,10 +9,12 @@ import {Product} from '../product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+products : Product[];
+  constructor(private _productService : ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe(response =>
+      this.products = response);
   }
 
 }
