9678bb35c1f37db2744212489ed6df9de9f39eff
Vijeth Sarashetti <M1053749@mindtree.com>
2020-11-30 12:09:27 +0530

    Task 3 update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
-import rxjs/add/operator/map;
+
 imports: [
   Http,
   Response
@@ -12,7 +12,7 @@ export class ProductService {
 private _albumUrl : '../assets/album.json'
 getAlbum(id: number)
 {
-   return this._http.get(this._albumUrl).response.json()
+   return this._http.get(this._albumUrl)
 
 }
 }
